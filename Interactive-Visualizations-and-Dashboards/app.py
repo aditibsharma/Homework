f7740eec63e873531478a681f17fb62eeeb635a5
AditiS <aditibidwai@gmail.com>
2018-05-07 20:01:36 -0500

    adding updated files

--- Interactive-Visualizations-and-Dashboards/app.py
+++ Interactive-Visualizations-and-Dashboards/app.py
@@ -1,4 +1,3 @@
-import datetime as dt
 import numpy as np
 import pandas as pd
 
@@ -14,122 +13,128 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 #################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
-
-#################################################
 # Database Setup
 #################################################
-from flask_sqlalchemy import SQLAlchemy
-# The database URI
-
-# Will set variable to the environment variable, DATABASE_URI, if it exists. Else, it sets to our dev database
-db_uri = os.getenv("DATABASE_URI", "sqlite:///belly_button_biodiversity.sqlite")
-
-engine = create_engine(db_uri)
+engine = create_engine("sqlite:///DataSets/belly_button_biodiversity.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-# Save references to each table
-Bellybutton = Base.classes.bellybutton
+# Save reference to the table
+otu = Base.classes.otu
+Samples = Base.classes.samples
+Samples_Metadata= Base.classes.samples_metadata
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-# Create database tables
-@app.before_first_request
-def setup():
-    # Recreate database each time for demo
-    # db.drop_all()
-    db.create_all()
-
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-
-    @app.route("/")
-    def home():
-    """Return the dashboard homepage."""
+# Returns the dashboard homepage
+@app.route("/")
+def index():
     return render_template("index.html")
 
+#################################################
+# Returns a list of sample names
+@app.route('/names')
+def names():
+    """Return a list of sample names."""
 
- @app.route("/names")
- def name_list():
-    """List of sample names."""
-return render_template()
+    # Use Pandas to perform the sql query
+    query1 = session.query(Samples).statement
+    df = pd.read_sql_query(query1, session.bind)
+    df.set_index('otu_id', inplace=True)
 
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns))
+
+#################################################
+# Returns a list of otu descriptions 
 @app.route('/otu')
-def OTU_list():
-    """List of OTU descriptions.'''
+def otu():
+    """Return a list of OTU descriptions."""
+    results = session.query(otu.lowest_taxonomic_unit_found).all()
 
+    # Use numpy ravel to extract list of tuples into a list of OTU descriptions
+    otu_list = list(np.ravel(results))
+    return jsonify(otu_list)
+
+#################################################
+# Returns a json dictionary of sample metadata 
 @app.route('/metadata/<sample>')
-def metadata():
-    """MetaData for a given sample.
+def sample_metadata(sample):
+    """Return the MetaData for a given sample."""
+    sel = [Samples_Metadata.SAMPLEID,
+           Samples_Metadata.ETHNICITY,
+           Samples_Metadata.GENDER,
+           Samples_Metadata.AGE, 
+           Samples_Metadata.LOCATION,
+           Samples_Metadata.BBTYPE, ]
 
+    # sample[3:] strips the `BB_` prefix from the sample name to match
+    # the numeric value of `SAMPLEID` from the database
+    results = session.query(*sel).\
+        filter(Samples_Metadata.SAMPLEID == sample[3:]).all()
+
+    # Create a dictionary entry for each row of metadata information
+    sample_metadata = {}
+    for result in results:
+        sample_metadata['SAMPLEID'] = result[0]
+        sample_metadata['ETHNICITY'] = result[1]
+        sample_metadata['GENDER'] = result[2]
+        sample_metadata['AGE'] = result[3]
+        sample_metadata['LOCATION'] = result[4]
+        sample_metadata['BBTYPE'] = result[5]
+
+    return jsonify(sample_metadata)
+
+#################################################
+# Returns an integer value for the weekly washing frequency `WFREQ`
 @app.route('/wfreq/<sample>')
-    """Weekly Washing Frequency as a number.
+def sample_wfreq(sample):
+    """Return the Weekly Washing Frequency as a number."""
 
+    # `sample[3:]` strips the `BB_` prefix
+    results = session.query(Samples_Metadata.WFREQ).\
+        filter(Samples_Metadata.SAMPLEID == sample[3:]).all()
+    wfreq = np.ravel(results)
 
+    # Return only the first integer value for washing frequency
+    return jsonify(int(wfreq[0]))
+
+#################################################
+# Return a list of dictionaries containing sorted lists  for `otu_ids`and `sample_values`
 @app.route('/samples/<sample>')
-    """OTU IDs and Sample Values for a given sample
-    # query for the top 10 emoji data
-    results = db.session.query(Emoji.emoji_char, Emoji.score).\
-        order_by(Emoji.score.desc()).\
-        limit(10).all()
+def samples(sample):
+    """Return a list dictionaries containing `otu_ids` and `sample_values`."""
+    query2 = session.query(Samples).statement
+    df = pd.read_sql_query(query2, session.bind)
 
-    # Select the top 10 query results
-    emoji_char = [result[0] for result in results]
-    scores = [int(result[1]) for result in results]
+    # Make sure that the sample was found in the columns, else throw an error
+    if sample not in df.columns:
+        return jsonify(f"Error! Sample: {sample} Not Found!"), 400
 
-    # Generate the plot trace
-    plot_trace = {
-        "x": emoji_char,
-        "y": scores,
-        "type": "bar"
-    }
-    return jsonify(plot_trace)
+    # Return any sample values greater than 1
+    df = df[df[sample] > 1]
 
+    # Sort the results by sample in descending order
+    df = df.sort_values(by=sample, ascending=0)
 
-
-@app.route("/emoji_id")
-def emoji_id_data():
-    """Return emoji score and emoji id"""
-
-    # query for the emoji data using pandas
-    query_statement = db.session.query(Emoji).\
-    order_by(Emoji.score.desc()).\
-    limit(10).statement
-    df = pd.read_sql_query(query_statement, db.session.bind)
+    # Format the data to send as json
+    data = [{
+        "otu_ids": df[sample].index.values.tolist(),
+        "sample_values": df[sample].values.tolist()
+    }]
+    return jsonify(data)
     
-    # Format the data for Plotly
-    plot_trace = {
-            "x": df["emoji_id"].values.tolist(),
-            "y": df["score"].values.tolist(),
-            "type": "bar"
-    }
-    return jsonify(plot_trace)
-
-@app.route("/emoji_name")
-def emoji_name_data():
-    """Return emoji score and emoji name"""
-
-    # query for the top 10 emoji data
-    results = db.session.query(Emoji.name, Emoji.score).\
-        order_by(Emoji.score.desc()).\
-        limit(10).all()
-    df = pd.DataFrame(results, columns=['name', 'score'])
-
-    # Format the data for Plotly
-    plot_trace = {
-            "x": df["name"].values.tolist(),
-            "y": df["score"].values.tolist(),
-            "type": "bar"
-    }
-    return jsonify(plot_trace)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
+   
