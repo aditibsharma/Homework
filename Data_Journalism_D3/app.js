42eb1390eee315fcea513f7da5824baed75fc83d
AditiS <aditibidwai@gmail.com>
2018-05-13 01:22:45 -0500

    tool tip updates.

--- Data_Journalism_D3/app.js
+++ Data_Journalism_D3/app.js
@@ -1,8 +1,11 @@
+
+// Set up our chart
+//= ================================
 var svgWidth = 960;
 var svgHeight = 500;
 
 var margin = {
-  top: 20,
+  top: 20 ,
   right: 40,
   bottom: 60,
   left: 100
@@ -34,7 +37,7 @@ d3.csv("./data/data.csv", function (err, unemploymentData) {
   // Step 2: Create scale functions
   // ==============================
   var xLinearScale = d3.scaleLinear()
-    .domain([20, d3.max(unemploymentData, d => d.UnemploymentRate)])
+    .domain([0, d3.max(unemploymentData, d => d.UnemploymentRate)])
     .range([0, width]);
 
   var yLinearScale = d3.scaleLinear()
@@ -66,12 +69,13 @@ d3.csv("./data/data.csv", function (err, unemploymentData) {
   .attr("r", "15")
   .attr("fill", "blue")
   .attr("opacity", ".5")
+  
 
   // Step 6: Initialize tool tip
   // ==============================
   var toolTip = d3.tip()
     .attr("class", "tooltip")
-    .offset([80, -60])
+    .offset([90, -60])
     .html(d =>
       `${d.state}<br>Unemployment Rate: ${d.UnemploymentRate}<br>High Confidence Level(Female): ${d.ConfidenceLimitHigh}`
     );
@@ -97,10 +101,13 @@ d3.csv("./data/data.csv", function (err, unemploymentData) {
     .attr("x", 0 - (height / 2))
     .attr("dy", "1em")
     .attr("class", "axisText")
-    .text("Confidence Level");
+    .text("Confidence Limit High");
 
   chartGroup.append("text")
     .attr("transform", `translate(${width/2}, ${height + margin.top + 30})`)
     .attr("class", "axisText")
     .text("Unemployement Rate for women under 25-44 years of age");
 });
+
+
+
